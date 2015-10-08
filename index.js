1aa00635e6c5375ab709300a2ce5f6388dfe8270
Fernando Fleury <me@fleury.io>
2015-10-08 16:21:27 -0300

    que UMD

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-'use strict;
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.queryObject = factory();
+  }
+})(this, function() {
+  'use strict';
 
-module.exports = function() {
   return 'que fita';
-};
+});
